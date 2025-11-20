ac86496df037b42123a7840ed2702bf69ff5b67a
ninelcatel <alexnegrisan0@gmail.com>
2025-11-20 13:48:49 +0000

    small css changes for livesearch bar in conversations

---
+++ Migrations/20251120134418_MigrationPostBug.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace unibucGram.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationPostBug : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
